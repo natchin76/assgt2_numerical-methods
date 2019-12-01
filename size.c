2fa4ec4d85216566fbad23e868e88d8293c138c0
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-01 21:35:31 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("Size of char:%d\n",sizeof(char));
+printf("Size of int:%d\n",sizeof(int));
+printf("Size of float:%d\n",sizeof(float));
+printf("Size of double:%d\n",sizeof(double));
+printf("Size of long int:%d\n",sizeof(long int));
+return(0);
+}
