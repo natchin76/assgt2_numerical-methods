8e1c2215e5209fc26c8c4161e820769ec00fece1
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-02 02:19:05 +0530

    Delete size.c

--- size.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("Size of char:%d\n",sizeof(char));
-printf("Size of int:%d\n",sizeof(int));
-printf("Size of float:%d\n",sizeof(float));
-printf("Size of double:%d\n",sizeof(double));
-printf("Size of long int:%d\n",sizeof(long int));
-return(0);
-}
