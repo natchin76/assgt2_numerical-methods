ce98161f3a55d38afc0b18e4272f0d7e6ac614db
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-01 23:30:06 +0530

    Output saved in  a txt file
    The output of code is saves in a file named output.txt

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include"mn_var.h"
 
 int main(){
+FILE *fp;
+fp=fopen("output.txt","w");	
 int i;
 float mv[2];
 float* a=(float*)malloc(100*sizeof(float));
@@ -15,8 +17,9 @@ a[i]=pow(i+1,2);
 }
 mv[0]=*stat(100,a);
 mv[1]=*(stat(100,a)+1);
-printf("Mean of the numbers is:%f\n",mv[0]);
-printf("Variance is:%f\n",mv[1]);
+fprintf(fp,"Mean of the numbers is:%f\n",mv[0]);
+fprintf(fp,"Variance is:%f\n",mv[1]);
+fclose(fp);
 return(0);
 free(a);
 }
