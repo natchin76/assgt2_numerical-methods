27359431b1fc4a5bd3a39548a9eb365b58b338e3
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-02 13:22:22 +0530

    Output is saved in a binary file

---
+++ otpt2bin.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include"mn_var.h"
+
+int main(){
+FILE *fp;
+fp=fopen("output.bin","wb");	
+int i;
+float mv[2];
+float* a=(float*)malloc(100*sizeof(float));
+float mean,var;
+
+for(i=0;i<100;i++)
+{
+a[i]=pow(i+1,2);
+}
+mv[0]=*stat(100,a);
+mv[1]=*(stat(100,a)+1);
+fprintf(fp,"Mean of the numbers is:%f\n",mv[0]);
+fprintf(fp,"Variance is:%f\n",mv[1]);
+fclose(fp);
+return(0);
+free(a);
+}
